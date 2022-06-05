7fc27ec2cc1366ad7a4e669c92d9b90dfcf6d4a0
PiotrSkor <106890500+PiotrSkor@users.noreply.github.com>
2022-06-05 12:48:28 +0200

    Add files via upload

---
+++ Recipe/Recipe/HelperMethods.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Recipe
+{
+
+
+    class HelperMethods
+    {
+        
+ 
+        public static void AddRecipeToListShowRecipe()
+        {
+
+            List<string> listOfProducts = new List<string>();
+            listOfProducts.Add("****************************************************");
+            Console.WriteLine("Podaj nazwę przepisu: ");
+            var NameOfRecipe = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("----------" + NameOfRecipe + "----------");
+            listOfProducts.Add(Convert.ToString(sb));
+
+
+
+        ValueRecipe:
+            Console.WriteLine("Ile składników chcesz dodać?");
+            var UserInput = Console.ReadLine();
+
+
+            if (int.TryParse(UserInput, out int id))
+            {
+                for (int i = 0; i < Convert.ToInt32(UserInput); i++)
+                {
+                    StringBuilder stringItem = new StringBuilder();
+                    Console.WriteLine($"Podaj {i + 1} produkt: ");
+                    var item = Console.ReadLine();
+                    Console.Write("\tPodaj gramature produktu: ");
+                    var valueOfProducts = Console.ReadLine();
+                    stringItem.Append(item + " - " + valueOfProducts);
+                    listOfProducts.Add(Convert.ToString(stringItem));
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Podaj prawidłową liczbę");
+                goto ValueRecipe;
+            }
+            listOfProducts.Add("****************************************************");
+            listOfProducts.Add("");
+            var filepath = @"D:\visua\Recipe\Przepisy\Recipe.txt";
+
+            File.AppendAllLines(filepath, listOfProducts);
+            Console.WriteLine("Dodano: ");
+            foreach (var items in listOfProducts)
+            {
+                Console.WriteLine(items);
+            }
+
+            
+
+            string recipeeeeee = JsonConvert.SerializeObject(listOfProducts);
+            File.WriteAllText(@"D:\visua\Recipe\Przepisy\recipeeeeee.json", recipeeeeee);
+            
+
+
+
+        }
+
+        public static void ShowRecipe()
+        {
+            var filepath = @"D:\visua\Recipe\Przepisy\Recipe.txt";
+            var textInside = File.ReadAllText(filepath);
+            Console.WriteLine(textInside);
+
+
+        }
+
+        public static void SearchRecipeIngredient()
+        {
+            Console.WriteLine("Po składniku");
+        }
+
+        public static void SearchRecipeName()
+        {
+            Console.WriteLine("Po nazwie");
+        }
+
+        public static void DeleteRecipe()
+        {
+            var filepath = @"D:\visua\Recipe\Przepisy\Recipe.txt";
+            var textInside = File.ReadAllText(filepath);
+
+            
+
+        }
+
+        /*JsonConvert.SerializeObject(listOfProducts);*/
+       
+
+    }
+
+    
+
+           
+}
---
+++ Recipe/Recipe/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Newtonsoft.Json;
+using static Recipe.HelperMethods;
+
+namespace Recipe
+{
+    public class Program 
+    {
+
+
+        public static void Main(string [] args)
+        {
+        Beginning:
+            Console.WriteLine("Co chcesz zrobić?\n");
+            Console.WriteLine("1.Dodać przepis");
+            Console.WriteLine("2.Wyświetlić przepis");
+            Console.WriteLine("3.Wyszukać przepis (nazwa)");
+            Console.WriteLine("4.Wyszukać przepis (składnik)");
+            Console.WriteLine("5.Usunąć przepis (numery)");
+            Console.WriteLine("0.Zamknij program");
+
+            var UserInput = Console.ReadLine();
+            int.TryParse(UserInput, out int value);
+            Console.Clear();
+            switch (value)
+            {
+                case 1:
+                    HelperMethods.AddRecipeToListShowRecipe();
+                    Console.WriteLine();
+                    
+                    goto Beginning;
+
+                    break;
+                case 2:
+                    
+                    HelperMethods.ShowRecipe();
+                    Console.WriteLine();
+                    
+                    goto Beginning;
+                    break;
+                case 3:
+                    HelperMethods.SearchRecipeName();
+                    Console.WriteLine();
+                    goto Beginning;
+                    break;
+                case 4:
+                    HelperMethods.SearchRecipeIngredient();
+                    Console.WriteLine();
+                    goto Beginning;
+                    break;
+                case 0:
+
+                    break;
+            }
+        }
+    }
+}
